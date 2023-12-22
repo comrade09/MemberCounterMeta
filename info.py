3321c2a41eab1e25ba54efc9f4543cf40c183c30
Comrade X <106185020+comrade09@users.noreply.github.com>
2023-12-23 02:53:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -2,3 +2,18 @@ import re
 from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
+
+
+CHANNEL_OR_GROUP_LIST = [i.strip() for i in os.environ.get("CHANNEL_OR_GROUP_LIST").split(' ')]
+CHANNEL_OR_GROUP_ID = int(os.environ["CHANNEL_OR_GROUP_ID"])
+MESSAGE_ID = int(os.environ["MESSAGE_ID"])
+API_ID = int(environ['API_ID'])
+API_HASH = environ['API_HASH']
+BOT_TOKEN = environ['BOT_TOKEN']
+SESSION_STRING = environ.get('SESSION_STRING')
+
+TIME_ZONE = os.environ["TIME_ZONE"]
+BOT_LIST = [i.strip() for i in os.environ.get("BOT_LIST").split(' ')]
+BOT_CHANNEL_OR_GROUP_ID = int(os.environ["CHANNEL_OR_GROUP_ID"])
+BOT_MESSAGE_ID = int(os.environ["MESSAGE_ID"])
+BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split(' ')]
